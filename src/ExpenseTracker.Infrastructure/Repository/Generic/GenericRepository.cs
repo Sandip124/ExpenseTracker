096676fd58ec52cbf05ddb694edfc76588f4efd0
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2021-07-03 15:24:58 +0545

    implement generic repository for async methods

--- src/ExpenseTracker.Infrastructure/Repository/Generic/GenericRepository.cs
+++ src/ExpenseTracker.Infrastructure/Repository/Generic/GenericRepository.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace ExpenseTracker.Infrastructure.Repository.Generic
@@ -12,40 +12,77 @@ namespace ExpenseTracker.Infrastructure.Repository.Generic
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
 
         public GenericRepository(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
         }
-        public void delete(T entity)
+        public void Delete(T entity)
         {
-            var dbset = _appDbContext.Set<T>().Remove(entity);
-            _appDbContext.SaveChanges();
+            _appDbContext.Set<T>().Remove(entity);
         }
 
-        public List<T> getAll()
+        public List<T> GetAll()
         {
             return _appDbContext.Set<T>().ToList();
         }
 
-        public T getById(long id)
+        public T GetById(long id)
         {
             return _appDbContext.Set<T>().Find(id);
         }
 
-        public IQueryable<T> getQueryable()
+        public async Task InsertAsync(T entity)
+        {
+            await _appDbContext.AddAsync(entity).ConfigureAwait(false);
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            predicate ??= x => true;
+            return await _appDbContext.Set<T>().Where(predicate).ToListAsync().ConfigureAwait(false);
+        }
+
+        public List<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _appDbContext.Set<T>().Where(predicate).ToList();
+        }
+
+        public async Task<T> GetItemAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _appDbContext.Set<T>().FirstOrDefaultAsync(predicate).ConfigureAwait(false);
+        }
+
+        public async Task<int> GetCountAsync(Expression<Func<T, bool>>? predicate)
+        {
+            predicate ??= x => true;
+            return await _appDbContext.Set<T>().CountAsync(predicate).ConfigureAwait(false);
+        }
+
+        public async Task<T> FindAsync(long id)
+        {
+            return await _dbSet.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<bool> CheckIfExistAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate).ConfigureAwait(false);
+        }
+        
+        public IQueryable<T> GetQueryable()
         {
             return _appDbContext.Set<T>();
         }
 
-        public void insert(T entity)
+        public void Insert(T entity)
         {
-           
-           _appDbContext.Set<T>().Add(entity);
+            _appDbContext.Set<T>().Add(entity);
            _appDbContext.SaveChanges();
         }
 
-        public void update(T entity)
+        public void Update(T entity)
         {
             _appDbContext.Entry(entity).State = EntityState.Modified;
             _appDbContext.Set<T>().Update(entity);
