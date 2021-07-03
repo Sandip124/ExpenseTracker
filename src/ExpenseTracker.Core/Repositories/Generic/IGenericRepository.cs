d7ed088a814a19d60301dc2113ab691a35f2d0e5
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2021-07-03 15:24:20 +0545

    change the method naming convention to pascal case and added async method to the GenericRepository.cs

--- src/ExpenseTracker.Core/Repositories/Generic/IGenericRepository.cs
+++ src/ExpenseTracker.Core/Repositories/Generic/IGenericRepository.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace ExpenseTracker.Core.Repositories.Generic
 {
    public interface IGenericRepository<T>
     {
-        void delete(T entity);
-        void insert(T entity);
-        void update(T entity);
-        List<T> getAll();
-        T getById(long id);
-        IQueryable<T> getQueryable();
+        void Delete(T entity);
+        void Insert(T entity);
+        void Update(T entity);
+        List<T> GetAll();
+        T GetById(long id);
+        Task InsertAsync(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null);
+        List<T> Get(Expression<Func<T, bool>> predicate);
+        Task<T> GetItemAsync(Expression<Func<T, bool>> predicate);
+        Task<int> GetCountAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FindAsync(long id);
+        Task<bool> CheckIfExistAsync(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetQueryable();
     }
 }
