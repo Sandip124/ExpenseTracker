08a292d8ed8dc2056885b884bb365f8d0362ea9f
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2021-07-03 18:38:23 +0545

    add razor runtime compilation

--- src/ExpenseTracker.Web/Startup.cs
+++ src/ExpenseTracker.Web/Startup.cs
@@ -24,6 +24,8 @@ namespace ExpenseTracker.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
