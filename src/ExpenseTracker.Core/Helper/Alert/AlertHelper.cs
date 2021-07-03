ba06c4a6ccd43af8d87201b8b8e4c297f0c0c786
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2021-07-03 16:32:00 +0545

    simplify alert helper  usages

--- src/ExpenseTracker.Core/Helper/Alert/AlertHelper.cs
+++ src/ExpenseTracker.Core/Helper/Alert/AlertHelper.cs
@@ -6,16 +6,16 @@ namespace ExpenseTracker.Core.Helper.Alert
     {
         public static void SetSuccessMessage(Controller controller, string message)
         {
-            Helper.Alert.Alert alert = new Helper.Alert.Alert {Message = message, MessageType = MessageType.Success};
+            Alert alert = new() {Message = message, MessageType = MessageType.Success};
         }
         
         public static void SetErrorMessage(Controller controller, string message)
         {
-            Helper.Alert.Alert alert = new Helper.Alert.Alert {Message = message, MessageType = MessageType.Error};
+            Alert alert = new() {Message = message, MessageType = MessageType.Error};
         }
         public static void SetInfoMessage(Controller controller, string message)
         {
-            Helper.Alert.Alert alert = new Helper.Alert.Alert {Message = message, MessageType = MessageType.Info};
+            Alert alert = new() {Message = message, MessageType = MessageType.Info};
         }
     }
 }
