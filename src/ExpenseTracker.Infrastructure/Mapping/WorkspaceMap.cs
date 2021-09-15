060e6a9e0902658e8b85b2b48d7edf4ac00e478b
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2021-09-15 05:50:10 +0545

    remove property mapping for workspace

--- src/ExpenseTracker.Infrastructure/Mapping/WorkspaceMap.cs
+++ src/ExpenseTracker.Infrastructure/Mapping/WorkspaceMap.cs
@@ -11,7 +11,6 @@ namespace ExpenseTracker.Infrastructure.Mapping
             Id(x => x.WorkspaceId).Column("workspace_id");
             Map(x => x.WorkSpaceName).CustomSqlType("varchar(100)").Not.Nullable().Column("name");
             Map(x => x.Color).CustomSqlType("varchar(50)").Not.Nullable().Column("color");
-            Map(x => x.BackgroundImage).CustomSqlType("varchar(50)").Not.Nullable().Column("background_image");
             Map(x => x.Description).CustomSqlType("text").Nullable().Column("description");
             Map(x => x.Token).CustomSqlType("varchar(50)").Not.Nullable().Column("token");
             Map(x => x.WorkspaceType).CustomSqlType("varchar(50)").Not.Nullable().Column("workspace_type");
